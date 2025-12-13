20bbed2870ef40e1558e8879a3948e57fbd67edd
alfahadiraihan <raihanhadi100@gmail.com>
2025-12-13 12:22:04 +0700

    feat: vercel.json generator

---
+++ src/scripts/generateVercelConfig.ts
@@ -0,0 +1,54 @@
+import cards from "../data/cards.json" with { type: "json" };
+import { writeFile } from "fs/promises";
+
+type VercelConfig = {
+  $schema: string;
+  headers: {
+    source: string;
+    headers: {
+      key: string;
+      value: string;
+    }[];
+  }[];
+  redirects: {
+    source: string;
+    destination: string;
+    permanent: boolean;
+  }[];
+}
+
+const firstSongSlug: string = cards[0].slug;
+
+const newConfig: VercelConfig = {
+  $schema: "https://openapi.vercel.sh/vercel.json",
+  headers: [
+    {
+      source: "/fonts/(.*)",
+      headers: [
+        {
+          "key": "Cache-Control",
+          "value": "public, max-age=31536000, immutable"
+        }
+      ]
+    }
+  ],
+  redirects: [
+    {
+      source: "/music",
+      destination: `/music/${firstSongSlug}`,
+      permanent: false
+    }
+  ]
+};
+
+function generateVercelConfig() {
+  writeFile(
+    "./vercel.json",
+    JSON.stringify(newConfig, null, 2),
+    "utf-8"
+  );
+  
+  console.log("Succesfully generated new vercel.json config.");
+}
+
+generateVercelConfig();
