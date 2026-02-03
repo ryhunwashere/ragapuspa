f573d3eaea3df433b4ba197933e891c08d2c0ed9
alfahadiraihan <raihanhadi100@gmail.com>
2026-02-03 23:00:26 +0700

    idk what to do anymore

--- astro.config.mjs
+++ astro.config.mjs
@@ -16,16 +16,5 @@ export default defineConfig({
       status: 301,
       destination: `/music/${firstSongSlug}`
     }
-  },
-
-  vite: {
-    define: {
-      'process.env.CLOUDINARY_CLOUD_NAME': JSON.stringify(process.env.CLOUDINARY_CLOUD_NAME),
-      'process.env.CLOUDINARY_API_KEY': JSON.stringify(process.env.CLOUDINARY_API_KEY),
-      'process.env.CLOUDINARY_API_SECRET': JSON.stringify(process.env.CLOUDINARY_API_SECRET),
-      'process.env.CLOUDINARY_FOLDER_GALLERY': JSON.stringify(process.env.CLOUDINARY_FOLDER_GALLERY),
-      'process.env.CLOUDINARY_FOLDER_MUSIC_TRACK': JSON.stringify(process.env.CLOUDINARY_FOLDER_MUSIC_TRACKS),
-      'process.env.CLOUDINARY_FOLDER_MUSIC_ARTWORKS': JSON.stringify(process.env.CLOUDINARY_FOLDER_MUSIC_ARTWORKS)
-    }
   }
 });
